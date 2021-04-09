f2c9a557fb06c415d2ca5ab6e70ca3ee395078c2
Logan Smith <logsmith06@gmail.com>
2021-04-09 16:44:36 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react';
 
 export default class ImageSlider extends Component {
 
+  constructor(props){
+    super()
+    this.state = {currentSlideIndex: 0}
+    
+  }
     render() {
 
 
