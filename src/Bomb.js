774e359a0df8271708a16f8dae84660c618f97e9
Logan Smith <logsmith06@gmail.com>
2021-04-09 16:47:19 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 
 export default class Bomb extends Component {
 
+constructor(props){
+  super()
+  this.state = {secondsLeft: props.initialCount}
+}
     render() {
-
+      return(
+        
+      )
 
     }
 }
